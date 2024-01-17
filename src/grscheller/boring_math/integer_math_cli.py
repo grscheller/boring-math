3c488460de6d8209d804a98aedfe440f65b14b8a
grscheller <geoffrey@scheller.com>
2024-01-17 13:32:27 -0700

    Updated documentation based on Markdown feedback.

--- src/grscheller/boring_math/integer_math_cli.py
+++ src/grscheller/boring_math/integer_math_cli.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Entry points for grscheller.boring_math.integer_math cli scripts
+"""Entry points for grscheller.boring_math.integer_math cli scripts.
 
-Supports OS independent CLI scripts demostrating the integer_math module.
+Supports automatically generated OS independent CLI scripts.
 """
 
 from __future__ import annotations
