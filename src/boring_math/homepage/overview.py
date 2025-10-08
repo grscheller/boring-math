67e0c3427731f1c1bae35b2169baa5c367083f43
grscheller <geoffrey@scheller.com>
2025-10-08 12:25:03 -0600

    Updated overview.py swapping PyPI with GH page for links.
    - for homepage

--- src/boring_math/homepage/overview.py
+++ src/boring_math/homepage/overview.py
@@ -5,7 +5,7 @@ The overall effort's name is
 - it is a collection of my mathematical hobby projects
 - each project's PyPI and GitHub names begin with *"boring-math-"*
 - each project implements a top-level package in the ``boring_math`` Python namespace
-- for links to PyPI project and GitHub repos `click here
-  <https://pypi.org/project/boring-math/>`_
+- for links to all the Boring Math project PyPI and GitHub repos `click here
+  <https://github.com/grscheller/boring-math>`_
 
 """
