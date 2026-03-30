5994586aff38bde00e5bb35bae9cd296cc54daa9
grscheller <geoffrey@scheller.com>
2026-03-30 17:13:57 -0600

    Beginning another attempt to standardize boring-math docs.
    - rebuilt all libraries
      - after updating __init__.py in all projects

--- src/boring_math/homepage/docs.py
+++ src/boring_math/homepage/docs.py
@@ -4,7 +4,7 @@
 - `boring-math-number-theory <https://grscheller.github.io/boring-math/number-theory/development/build/html/>`_
 - `boring-math-probability-distributions <https://grscheller.github.io/boring-math/probability-distributions/development/build/html/>`_ 
 - `boring-math-pythagorean-triples <https://grscheller.github.io/boring-math/pythagorean-triples/development/build/html/>`_
-- `recursive-functions <https://grscheller.github.io/boring-math/recursive-functions/development/build/html/>`_
+- `boring-math-recursive-functions <https://grscheller.github.io/boring-math/recursive-functions/development/build/html/>`_
 - `boring-math-special-functions <https://grscheller.github.io/boring-math/special-functions/development/build/html/>`_
 
 """
